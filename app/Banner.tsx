f55d0272172bd6f1391412d14575de6397dfe08f
nicolas jolys <jolys.nicolas@gmail.com>
2023-05-17 10:37:06 +0200

    Fix escaping characters

--- app/Banner.tsx
+++ app/Banner.tsx
@@ -1,7 +1,7 @@
 export default function Banner() {
     return (
       <div className="bg-gray-300 py-32 space-y-12">
-        {/* <div className="mx-auto max-w-7xl px-6 lg:flex lg:items-center lg:justify-between lg:px-8">
+        <div className="mx-auto max-w-7xl px-6 lg:flex lg:items-center lg:justify-between lg:px-8">
           <h2 className="text-3xl font-bold tracking-tight text-red-950 sm:text-4xl">
             <p className="">Un devis, une demande, un événement ?</p>
             <p className="">Contactez-nous</p>
@@ -18,9 +18,9 @@ export default function Banner() {
           </div>
         </div>
         <div className="mx-auto max-w-7xl px-6">
-          <p className="italic text-white font-medium">Passages se dévoue à faire exister des événements qui vous ressemblent. Peu importe votre projet, nous sommes là pour vous assurer une prestation sur-mesure. Vous voulez organiser un mariage végétarien ? Un cocktail en plein-air, ou un banquet à la ferme ? Un repas d'entreprise en format gastronomique ? Un dîner privé, à six à la maison ou dans un cadre atypique ? Un catering adapté à toutes les restrictions alimentaires pour le tournage de votre film ? Suprendre des amis avec un brunch en formule omakase ? Un goûter d'anniversaire pour grands enfants amateurs de pâtisseries fines ? </p>
-          <p className="italic text-white font-medium">Sur Rennes, en Bretagne, ou même plus loin, n'hésitez pas à prendre contact avec nous pour donner vie à vos projets. </p>
-        </div> */}
+          <p className="italic text-white font-medium">Passages se dévoue à faire exister des événements qui vous ressemblent. Peu importe votre projet, nous sommes là pour vous assurer une prestation sur-mesure. Vous voulez organiser un mariage végétarien ? Un cocktail en plein-air, ou un banquet à la ferme ? Un repas d&apos;entreprise en format gastronomique ? Un dîner privé, à six à la maison ou dans un cadre atypique ? Un catering adapté à toutes les restrictions alimentaires pour le tournage de votre film ? Suprendre des amis avec un brunch en formule omakase ? Un goûter d&apos;anniversaire pour grands enfants amateurs de pâtisseries fines ? </p>
+          <p className="italic text-white font-medium">Sur Rennes, en Bretagne, ou même plus loin, n&apos;hésitez pas à prendre contact avec nous pour donner vie à vos projets. </p>
+        </div>
       </div>
     )
   }
