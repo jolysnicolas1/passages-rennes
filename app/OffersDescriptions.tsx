9df203beb5fb1bbff97c03629e1d2b986dac90c6
nicolas jolys <jolys.nicolas@gmail.com>
2023-05-21 16:48:51 +0200

    Add motion lib in offers

--- app/OffersDescriptions.tsx
+++ app/OffersDescriptions.tsx
@@ -1,12 +1,3 @@
-import Image from "next/image"
-import offre1 from "../public/offres1.webp"
-import offre2 from "/public/offres2.webp"
-import offre3 from "/public/offres3.webp"
-import offre4 from "/public/offres4.webp"
-import OffersDescriptions from "./OffersDescriptions"
-import OffersPictures from "./OffersPictures"
-import 
-
 const features = [
   { name: "Cocktails et réceptions ", 
     description: "De bouchées délicates, des boissons innovantes, un service impeccable… Offrez-vous une soirée réussie." },
@@ -24,11 +15,20 @@ const features = [
   },
 ]
 
-export default function Offers() {
+export default function OffersDescriptions() {
+  
   return (
-      <motion.div className="mx-auto grid h-fit max-w-2xl grid-cols-1 items-center gap-x-12 gap-y-16 px-4 sm:px-6 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
-        <OffersPictures></OffersPictures>
-        <OffersDescriptions></OffersDescriptions>
-      </motion.div>
+        <div className="max-w-2xl min-h-full flex flex-col justify-between pb-24">
+          <h2 className="py-3 text-3xl font-bold tracking-tight text-lime-950 sm:text-4xl my-5">
+            Nos offres
+          </h2>
+          <dl className="space-y-12">
+            {features.map((feature) => (
+              <div key={feature.name} className="border-t border-gray-200 pt-4">
+                <dt className="font-medium text-lime-950 text-xl italic">{feature.name}</dt>
+              </div>
+            ))}
+          </dl>
+        </div>
   )
 }
