b14b53a39e645073fb1595e61c545fe9a23073ee
nicolas jolys <jolys.nicolas@gmail.com>
2023-05-21 16:02:11 +0200

    Create components for hero section

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -1,64 +1,15 @@
-import logo from '/public/logo_passages_vert.png'
-import Image from 'next/image'
+import HeroContent from './HeroContent'
+import HeroBgOne from './HeroBgOne'
+import HeroBgTwo from './HeroBgTwo'
 
 
 export default function Hero() {
     return (
       <div className="bg-white w-full">
         <div className="relative isolate pt-14">
-          <div
-            className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-            aria-hidden="true"
-          >
-            <div
-              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#3FA535] to-[#3FA535] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-              style={{
-                clipPath:
-                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-              }}
-            />
-          </div>
-          <div className="py-24 sm:py-32 lg:pb-40">
-            <div className="mx-auto px-6 lg:px-8 space-y-12">
-              <div className="mx-auto max-w-4xl text-center space-y-12">
-                <div className="flex items-center">
-                    <Image
-                        className="h-44 w-auto hidden md:inline"
-                        src={logo}
-                        alt="Picture of the author"
-                        width={300}
-                        height={300}
-                    />
-                  <h1 className="text-4xl font-medium tracking-tight text-lime-950 sm:text-6xl">
-                  Passages <strong className="passage font-extralight">gastronomie itinérante et sur-mesure</strong>
-                  </h1>
-                </div>
-                <p className="mt-6 text-lg leading-8 text-gray-600">
-                Notre équipe de passionnés, basée sur Rennes, vous accompagne dans l’élaboration de vos évènements en proposant des solutions de restauration personnalisées . Contactez-nous pour déterminer ensemble l’offre qui vous conviendra le mieux !
-                </p>
-                <div className="mt-10 flex items-center justify-center gap-x-6">
-                  <a
-                    href="/contact"
-                    className="rounded-md bg-red-600 px-4 py-3 text-md font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
-                  >
-                    Nous contacter
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div
-            className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-            aria-hidden="true"
-          >
-            <div
-              className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#3FA535] to-[#3FA535] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-              style={{
-                clipPath:
-                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-              }}
-            />
-          </div>
+          <HeroBgOne></HeroBgOne>
+          <HeroContent></HeroContent>
+          <HeroBgTwo></HeroBgTwo>
         </div>
       </div>
     )
