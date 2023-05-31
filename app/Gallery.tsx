3ccf0ffc8e7a857014cbdf12c706a326128bc426
nicolas jolys <jolys.nicolas@gmail.com>
2023-05-31 14:49:22 +0200

    test apostrophes

--- app/Gallery.tsx
+++ app/Gallery.tsx
@@ -1,279 +1,37 @@
 import Image from "next/image";
 import offre4 from "/public/offres4.webp"
 
+const ImageCol = ({images}: {images: string[]}) => {
+  return <>{images.map(image => <div           key={image}
+    ><Image
+    
+              className=""
+              src={image}
+              width={300}
+              height={1000}
+              alt=""
+              quality={100}
+            /></div>)}</>
+}
+
+const images = ["/portfolio10.jpg", "/portfolio11.jpg", "/portfolio12.jpg", "/portfolio13.jpg", "/portfolio10.jpg", "/portfolio11.jpg"]
 
 export default function Gallery() {
   return (
     <div className="mx-auto max-w-4xl flex space-x-4 m-5 mb-12">
+
       <div className='flex flex-col space-y-4'>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio10.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-          <Image
-          className=""
-          src="/portfolio11.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio12.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio13.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio19.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio15.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio29.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio26.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio27.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
+        <ImageCol images={images} />
       </div>
 
       <div className='flex flex-col space-y-4'>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio01.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-          <Image
-          className=""
-          src="/portfolio02.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio03.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio04.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio05.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio07.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio08.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio09.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio28.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
+        <ImageCol images={images} />
+      </div>
+      
+      <div className='flex flex-col space-y-4'>
+        <ImageCol images={images} />
       </div>
 
-      <div className='flex flex-col space-y-4'>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio17.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-          <Image
-          className=""
-          src="/portfolio16.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio18.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio14.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio20.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio21.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio24.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio22.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio23.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio25.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-        <div className=''>
-        <Image
-          className=""
-          src="/portfolio30.jpg"
-          width={300}
-          height={1000}
-          alt=""
-        />
-        </div>
-      </div>
     </div>
   )
 }
